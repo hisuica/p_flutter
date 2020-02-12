c05406bef78f1937f0446ee39a993751aee79f34
hisuica <hisuica@gmail.com>
2020-02-12 19:08:28 +0900

    MusicListView作成

--- lib/ui/music/music_list.dart
+++ lib/ui/music/music_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p_flutter/widget/app_icon_widget.dart';
 
 class MusicListScreen extends StatefulWidget {
   const MusicListScreen({Key key}) : super(key: key);
@@ -10,8 +11,29 @@ class MusicListScreen extends StatefulWidget {
 class _MusicListScreenState extends State<MusicListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Music Page')),
-    );
+    return _buildTop100List();
   }
+
+  Widget _buildTop100List() => ListView.separated(
+    padding: EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 0),
+    itemCount: 100,
+    itemBuilder: (BuildContext context, int index) => _tile(index + 1),
+    separatorBuilder: (context, index) => Divider(),
+  );
+
+  ListTile _tile(int rank) => ListTile(
+    title: Text('Title'),
+    subtitle: Text('Subtitle'),
+    leading: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          width: 40.0,
+          child: Text(rank.toString(), style: TextStyle(fontSize: 17.0), textAlign: TextAlign.center,),
+        ),
+        AppIconWidget(image: 'assets/icons/ic_appicon.png',)
+      ],
+    ),
+    trailing: Icon(Icons.favorite_border),
+  );
 }
